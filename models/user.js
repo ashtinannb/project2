480b5b1934db9a64d4cdc8f22d2800a0ca6c44d6
mitchgreene92 <mitchgreene92@gmail.com>
2020-05-09 13:17:40 -0500

    worked on models, account creation, updated gitignore, server

---
+++ models/user.js
@@ -0,0 +1,54 @@
+// Require bcrypt for password hashing
+var bcrypt = require("bcrypt-nodejs");
+
+//user model with email, password, name and profile picture from URL only
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define("User", {
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    name: {
+      type: DataTypes.STRING,
+      defaultValue: "Anonymous",
+      allowNull: true
+    },
+    profileImage: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        isUrl: true,
+      },
+    },
+  });
+
+  // upon user creation, automatically hash their password
+  User.hook("creatingUser", function(user) {
+    user.password = bcrypt.hashSync(
+      user.password,
+      bcrypt.genSaltSync(10),
+      null
+    );
+  });
+
+ // ensure password can be compared to hashed password
+ User.prototype.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+  };
+
+  User.associate = function(models) {
+    models.User.hasMany(models.notes, {
+      onDelete: "CASCADE"
+    })
+  };
+  
+  return User;
+};
