9c8eb27f09400ff2558fbbde183021795f4c783e
Shayda Bakhshi <shayda.bakhshi@gmail.com>
2020-05-09 15:12:06 -0500

    created DB folder for .sql files; created basic functionalities for primary apiRoutes.js and htmlRoutes.js

--- server.js
+++ server.js
@@ -12,31 +12,32 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-app.use(session({secret: "this is a secret", resave: true, saveUninitialized: true}));
+app.use(session({ secret: "this is a secret", resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 
-// Routes
+// Routesß
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-var syncOptions = { force: false };
+
+var syncOptions = { force: process.env.FORCE === "true" ? true : false };
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
 if (process.env.NODE_ENV === "test") {
-  syncOptions.force = true;
+    syncOptions.force = true;
 }
 
 // Starting the server, syncing our models ------------------------------------/
 db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
-    console.log (
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
-  });
+    app.listen(PORT, function() {
+        console.log(
+            "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+            PORT,
+            PORT
+        );
+    });
 });
 
 module.exports = app;
