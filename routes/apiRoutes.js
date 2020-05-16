1df2a9ab59747a71331468ad3126c9db330f8089
Shayda Bakhshi <shayda.bakhshi@gmail.com>
2020-05-15 20:45:03 -0500

    added several user-based API routes to apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,6 +10,13 @@ module.exports = function(app) {
     });
   });
 
+    // Get all users
+    app.get("/api/user", function(req, res) {
+      db.Notes.findAll({}).then(function(dbUsers) {
+        res.json(dbUsers);
+      });
+    });
+
 
   // Get notes by id
   app.get("/api/notes/id/:id", function(req, res) {
@@ -61,6 +68,14 @@ module.exports = function(app) {
     });
   });
 
+
+    // Create a new user
+    app.post("/api/user", function(req, res) {
+      db.User.create(req.body).then(function(dbUser) {
+        res.json(dbUser);
+      });
+    });
+
   // Delete a notes post by id
   app.delete("/api/notes/:id", function(req, res) {
     db.Notes.destroy({
@@ -97,5 +112,93 @@ module.exports = function(app) {
             res.json(dbNotes);
         });
     });
+
+
+
+
+
+
+
+    app.get("/api/user/count/:name", function(req, res){
+      db.User.count({
+        where: { email: req.params.name }
+      })
+        .then(result, function(req, res) {
+          // console.log(result);
+          res.json(result);
+        })
+        .catch(function(err) {
+          console.log(err);
+          res.json(err);
+        });
+    });
+
+
+app.post('/api/signup', (req, res) => {
+    db.User.create({
+      username: req.body.username,
+      password: req.body.password
+    })
+      .then(function() {
+        res.redirect(307, "/api/login");
+      })
+      .catch(function(err) {
+        console.log(err);
+        res.json(err);
+      });
+  });
+
+
+  app.get("/api/user_data", (req, res) => {
+    if (!req.user) {
+      res.json({});
+    } else {
+      res.json({
+        user: { id: req.user.id, name: req.user.name }
+      });
+    }
+  }); 
+
+  app.get("/api/user/:id", (req, res) => {
+    db.User.findOne({
+      attributes: ["id", "name"],
+      where: {
+        id: req.params.id
+      }
+    }).then(name => {
+      res.json(name);
+    });
+  });
+
+  app.put("/api/user/name/:id", (req, res) => {
+    db.User.update(
+      {
+        name: req.body.name
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    ).then(name => {
+      res.json(name);
+    });
+  });
+
+
+
+app.get("/logout", (req, res) => {
+    req.logout();
+    res.redirect("/");
+  });
+
+
+    // Authenticate user
+    app.post("/api/login", passport.authenticate("local"), function(req, res) {
+      res.json("/user/profile");
+    }); 
+
+
+
 }
 
