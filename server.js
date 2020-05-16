edf615923577a70ef63b5af9e2b80989d8a57667
Ashtin Brannon <ashtinannbrannon@gmail.com>
2020-05-16 11:31:29 -0500

    connecting mysql

--- server.js
+++ server.js
@@ -6,6 +6,20 @@ var app = express();
 var session = require("express-session");
 var passport = require("./config/passport");
 
+var mysql = require('mysql');
+var connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "pw",
+        database: "notes_db"
+    });
+}
+
 var PORT = process.env.PORT || 3000;
 
 // Middleware
